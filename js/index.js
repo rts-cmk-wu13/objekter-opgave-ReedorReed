2bc1bdc6f4c186b4aa1901ac2723e63066cd2d86
Christian Reed <reed@Christians-MacBook-Pro.local>
2025-08-26 11:26:13 +0200

    added html through javascript with data from data.js objects

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // slå dig løs her...
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
-services.forEach((service) => console.log(service.headline));
+//services.forEach((service) => console.log(service.headline));
 
 const wrapper = document.querySelector('.wrapper');
 const headerSection = document.querySelector('.header');
@@ -11,3 +11,48 @@ const facilitiesSection = document.querySelector('.facilities');
 const sitesSection = document.querySelector('.sites');
 const advantagesSection = document.querySelector('.advantages');
 const footerSection = document.querySelector('.footer');
+
+console.log();
+
+handleHero();
+handleServices();
+handleFacilities();
+handleSites();
+handleAdvantages();
+handleFooter();
+
+//Hero section
+
+function handleHero() {
+	const heroContent = /*html */ `
+    <article class="hero__content">
+        <img src="${hero.image}" class="hero__img">
+        <div class="hero__textContent">
+            <h2 class="hero__heading">${hero.headline}</h2>
+            <p class="hero__text">${hero.copy}</p>
+            <a href="#" class="hero__btn"><img src="${hero.icon}">Explore</a>
+        </div>
+    </article>
+    `;
+
+	heroSection.insertAdjacentHTML('afterbegin', heroContent);
+}
+
+// Services section
+
+function handleServices() {
+	const servicesContent = services.map((service) => {
+	return	/*html */ `
+    <article class="services__cards">
+        <img src="${service['illustration']}" class="services__img">
+        <div class="services__cardText">
+            <h2 class="services__heading">${service.headline}</h2>
+            <p class="services__text">${service.text}</p>
+            <a href="#" class="services__link">${service.linktext}</a>
+        </div>
+    </article>
+    `;
+	}).join('');
+
+	servicesSection.insertAdjacentHTML('afterbegin', servicesContent);
+}
