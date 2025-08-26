185b31ca82166db367a823eae3ba95897b8aef83
Christian Reed <reed@Christians-MacBook-Pro.local>
2025-08-26 08:51:30 +0200

    made a branch for the build and got all the html tags into the index.js file

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-// slå dig løs her... 
+// slå dig løs her...
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
-services.forEach(service => console.log(service.headline))
+services.forEach((service) => console.log(service.headline));
 
-
+const wrapper = document.querySelector('.wrapper');
+const headerSection = document.querySelector('.header');
+const heroSection = document.querySelector('.hero');
+const servicesSection = document.querySelector('.services');
+const facilitiesSection = document.querySelector('.facilities');
+const sitesSection = document.querySelector('.sites');
+const advantagesSection = document.querySelector('.advantages');
+const footerSection = document.querySelector('.footer');
